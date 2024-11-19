10eb9f955baca59dffb5fa0dcbce7d5e54d24c4c
OksanaGashkova <komyshewa@mail.ru>
2024-11-19 21:04:47 +0500

    исправлены строки

--- script/script.js
+++ script/script.js
@@ -19,7 +19,7 @@ const barbos = [];
 const arr = [sharik, bobik, tusik, strelka, belka, graf, tsigan, juk, rex, bim, barbos];
 
 for (let i = 0; i < result.length; i++) {
-    debugger
+    
     switch (result[i]) {
        
        case 'Шарик':
@@ -84,21 +84,21 @@ console.log('самая часто повторяющаяся кличка: ' + 
 let names = [sharik.length, bobik.length, tusik.length, strelka.length, belka.length, graf.length, tsigan.length, juk.length, rex.length, bim.length, barbos.length];
 
 let max = Math.max(...names);
-console.log(BigName[0] + ':' + ' ' + max);
+console.log(BigName[0] + ': ' + max);
 
 console.log('количество кличек без учета повторов' + ':' + ' ' + names.length);
 
-console.log('Шарик' + ':' + ' ' + sharik.length);
-console.log('Бобик' + ':' + ' ' + bobik.length);
-console.log('Тузик' + ':' + ' ' + tusik.length);
-console.log('Стрелка' + ':' + ' ' + strelka.length);
-console.log('Белка' + ':' + ' ' + belka.length);
-console.log('Граф' + ':' + ' ' + graf.length);
-console.log('Цыган' + ':' + ' ' + tsigan.length);
-console.log('Жук' + ':' + ' ' + juk.length);
-console.log('Рекс' + ':' + ' ' + rex.length);
-console.log('Бим' + ':' + ' ' + bim.length);
-console.log('Барбос' + ':' + ' ' + barbos.length);
+console.log('Шарик: ' + sharik.length);
+console.log('Бобик: ' + bobik.length);
+console.log('Тузик: ' + tusik.length);
+console.log('Стрелка: ' + strelka.length);
+console.log('Белка: ' + belka.length);
+console.log('Граф: ' + graf.length);
+console.log('Цыган: ' + tsigan.length);
+console.log('Жук: ' + juk.length);
+console.log('Рекс: ' + rex.length);
+console.log('Бим: ' + bim.length);
+console.log('Барбос: ' + barbos.length);
 
 
 
